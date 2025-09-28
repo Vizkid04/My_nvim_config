aa58f1f4b67554617608f3db5d44e98a11d15621
Vizkid04 <chesskid04@gmail.com>
2025-09-28 21:21:55 +0530

    latex_conceal_update

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -7,6 +7,23 @@ return {
             vim.cmd([[let g:vimtex_compiler_method = 'latexmk']])
             vim.cmd([[map <C-l> \ll<CR> ]])
             vim.cmd([[let g:vimtex_quickfix_open_on_warning = 0]])
+            vim.opt.conceallevel = 0
+            vim.opt.concealcursor = ""
+            vim.g.vimtex_syntax_conceal = {
+                accents = 0,
+                ligatures = 0,
+                cites = 0,
+                fancy = 0,
+                spacing = 0,
+                greek = 0,
+                math_bounds = 0,
+                math_delimiters = 0,
+                math_fracs = 0,
+                math_super_sub = 0,
+                math_symbols = 0,
+                sections = 0,
+                styles = 0,
+            }
         end,
     },
 }
