ad05f23a4c98507a4a701032756caa28e96dd387
Vizkid04 <chesskid04@gmail.com>
2025-10-10 03:10:10 +0530

    reverse search okular latex

--- lua/plugins/latex.lua
+++ lua/plugins/latex.lua
@@ -2,27 +2,36 @@ return {
     {
         "lervag/vimtex",
         config = function()
-            vim.cmd([[let g:vimtex_view_general_viewer = 'okular']])
-            vim.cmd([[let g:vimtex_view_general_options = '--unique file:@pdf\#src:@line@tex']])
-            vim.cmd([[let g:vimtex_compiler_method = 'latexmk']])
-            vim.cmd([[map <C-l> \ll<CR> ]])
-            vim.cmd([[let g:vimtex_quickfix_open_on_warning = 0]])
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "tex",
+                callback = function()
+                    local project = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+                    local socket = "/tmp/nvim-" .. project .. "-" .. vim.fn.getpid()
+                    if vim.v.servername == "" then
+                        vim.fn.serverstart(socket)
+                        print("Neovim server started at: " .. socket)
+                    end
+                    vim.g.vimtex_nvr_socket = vim.v.servername ~= "" and vim.v.servername or socket
+                    vim.keymap.set("n", "<C-l>", "<cmd>VimtexCompile<CR>", { buffer = true, silent = true })
+                    vim.keymap.set("n", "<leader>lv", "<cmd>VimtexView<CR>", { buffer = true, silent = true })
+                end,
+            })
+            vim.g.vimtex_view_method = "general"
+            vim.g.vimtex_view_general_viewer = "okular"
+            vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
+            vim.g.vimtex_compiler_method = "latexmk"
+            vim.g.vimtex_compiler_latexmk = {
+                options = { "-pdf", "-synctex=1", "-interaction=nonstopmode", "-file-line-error" },
+            }
+            vim.g.vimtex_compiler_continuous = 1
+            vim.g.vimtex_quickfix_open_on_warning = 0
             vim.opt.conceallevel = 0
             vim.opt.concealcursor = ""
             vim.g.vimtex_syntax_conceal = {
-                accents = 0,
-                ligatures = 0,
-                cites = 0,
-                fancy = 0,
-                spacing = 0,
-                greek = 0,
-                math_bounds = 0,
-                math_delimiters = 0,
-                math_fracs = 0,
-                math_super_sub = 0,
-                math_symbols = 0,
-                sections = 0,
-                styles = 0,
+                accents = 0, ligatures = 0, cites = 0, fancy = 0,
+                spacing = 0, greek = 0, math_bounds = 0, math_delimiters = 0,
+                math_fracs = 0, math_super_sub = 0, math_symbols = 0,
+                sections = 0, styles = 0,
             }
         end,
     },
