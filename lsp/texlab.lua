209371b6637a1a1d91bd1f0a30ad0b68c888b547
Vizkid04 <chesskid04@gmail.com>
2026-03-05 15:26:27 +0530

    Added project root detection to texlab.

--- lsp/texlab.lua
+++ lsp/texlab.lua
@@ -1,9 +1,25 @@
 return {
-  cmd = {'texlab'},
-  filetypes = {'tex'},
-  root_dir = function(bufnr, on_dir)
-    local path = vim.api.nvim_buf_get_name(bufnr)
-    local dir = vim.fn.fnamemodify(path, ":p:h")
-    on_dir(dir)
+  cmd = { 'texlab' },
+  filetypes = { 'tex', 'bib' },
+  root_dir = function(fname)
+    local path = (type(fname) == 'string' and fname ~= "") and fname or vim.api.nvim_buf_get_name(0)
+    if path == "" then return vim.fn.getcwd() end
+    local root_files = { '.git', 'latexmkrc', '.latexmkrc', 'main.tex', 'index.tex' }
+    local search = vim.fs.find(root_files, { upward = true, path = path })
+    if #search > 0 then
+      return vim.fs.dirname(search[1])
+    end
+    return vim.fs.dirname(path)
   end,
+  settings = {
+    texlab = {
+      build = { onSave = true },
+      auxDirectory = ".",
+      bibDirectory = ".",
+      chktex = {
+        onOpenAndSave = true,
+        onEdit = false
+      },
+    },
+  },
 }
